60a381537e55eed85d46035f8a73e489f8638a2c
Jeevan Kishore <jeevan@quintype.com>
2018-03-20 17:49:22 +0530

    Configurable widget

--- metype/metype-widget-template.php
+++ metype/metype-widget-template.php
@@ -4,9 +4,9 @@
 <div id='metype-container' class='iframe-container'
      data-metype-account-id='<?php echo get_option('metype-account-id'); ?>'
      data-metype-host='https://staging.metype.com/'
-     data-metype-primary-color='primary color of the widget (defaults to blue)'
-     data-metype-bg-color='#ffffff'
-     data-metype-font-color='#4a4a4a'>
+     data-metype-primary-color='<?php echo get_option('metype-primary-color'); ?>'
+     data-metype-bg-color='<?php echo get_option('metype-bg-id'); ?>'
+     data-metype-font-color='<?php echo get_option('metype-font-id'); ?>'>
 </div>
 
 <script type='text/javascript'>
