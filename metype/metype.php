34a02ee0e083403cba516d6133efcccd012feefd
Jeevan Kishore <jeevan@quintype.com>
2018-03-20 16:58:38 +0530

    #Feed integration

--- metype/metype.php
+++ metype/metype.php
@@ -3,48 +3,66 @@
 *Plugin Name: Metype Engagement-engine
 */
 function metype_moderate_comments_render() {
-	require_once plugin_dir_path( __FILE__ ) . '/metype-admin.php';
+    require_once plugin_dir_path( __FILE__ ) . '/metype-admin.php';
 }
 
-function metype_contruct_admin_menu() {
-	if ( ! current_user_can( 'moderate_comments' ) ) {
-		return;
-	}
+function metype_construct_admin_menu() {
+    if ( ! current_user_can( 'moderate_comments' ) ) {
+        return;
+    }
 
-        // Replace the existing WordPress comments menu item to prevent confusion
-        // about where to administer comments. The Disqus page will have a link to
-        // see WordPress comments.
-	remove_menu_page( 'edit-comments.php' );
+    // Replace the existing WordPress comments menu item to prevent confusion
+    // about where to administer comments. The Disqus page will have a link to
+    // see WordPress comments.
+    remove_menu_page( 'edit-comments.php' );
 
-	add_menu_page(
-		'metype',
-		'Metype',
-		'moderate_comments',
-		'metype',
-		'metype_moderate_comments_render',
-		'dashicons-admin-comments',
-		24
-	);
-	add_action( 'admin_init', 'update_extra_post_info' );
+    add_menu_page(
+        'metype',
+        'Metype',
+        'moderate_comments',
+        'metype',
+        'metype_moderate_comments_render',
+        'dashicons-admin-comments',
+        24
+    );
+    add_action( 'admin_init', 'update_extra_post_info' );
 }
 
 function update_extra_post_info() {
-  register_setting( 'metype-settings', 'metype-account-id' );
+    register_setting( 'metype-settings', 'metype-account-id' );
 }
 
 function metype_construct_admin_bar( $wp_admin_bar ) {
-	$wp_admin_bar->remove_node( 'comments' );
+    $wp_admin_bar->remove_node( 'comments' );
 }
 
 function my_plugin_comment_template( $comment_template ) {
-	return dirname(__FILE__) . '/h1.php';
+    return dirname(__FILE__) . '/metype-widget-template.php';
 }
 
 function add_metype_script() {
     echo '<script type=\'text/javascript\'>window.talktype = window.talktype || function(f) {(talktype.q = talktype.q || []).push(arguments);};</script><script src=\'https://staging.metype.com/quintype-metype/assets/application.js\'></script>';
 }
 
+//Add application.js to the head of the page
 add_action('wp_head', 'add_metype_script');
-add_action( 'admin_menu', 'metype_contruct_admin_menu' );
+
+//Construct admin menu
+add_action( 'admin_menu', 'metype_construct_admin_menu' );
+
+//Insert custom comment template
 add_filter( "comments_template", "my_plugin_comment_template" );
+
+
+//Add feedwidget js to page
+wp_enqueue_script( 'feed_widget_js', plugin_dir_url( __FILE__ ) . 'scripts/feed_widget.js' );
+
+//Add feedwidget css to page
+wp_enqueue_style( 'feed_widget_css', plugin_dir_url( __FILE__ ) . 'styles/feed_widget.css' );
+
+
+wp_localize_script( 'feed_widget_js', 'php_vars', array(
+    'metypeAccountId' => get_option('metype-account-id')
+));
+
 ?>
---
+++ metype/scripts/feed_widget.js
@@ -0,0 +1,41 @@
+(function insert_feed_widget() {
+
+  function slideToggle() {
+    talktype && talktype.slideButton();
+  }
+
+  function toggleButton() {
+    talktype && talktype.toggleButton();
+  }
+
+  window.onload = function() {
+    var elemDiv = document.createElement('div');
+    elemDiv.setAttribute('id', 'feed-metype-container');
+    elemDiv.setAttribute('class', 'feed-iframe-container');
+    elemDiv.setAttribute('data-metype-account-id', php_vars.metypeAccountId );
+    elemDiv.setAttribute('data-metype-host', 'https://staging.metype.com/');
+
+    var clickThruDiv = document.createElement('div');
+    clickThruDiv.setAttribute('id', 'metype-clickthru');
+    clickThruDiv.setAttribute('class', 'metype-clickthru');
+    clickThruDiv.addEventListener('click', toggleButton, false);
+
+    var slideIconDiv = document.createElement('div');
+    slideIconDiv.setAttribute('id', 'metype-feed-slide-icon');
+    slideIconDiv.setAttribute('class', 'metype-feed-slide-icon');
+    slideIconDiv.addEventListener('click', slideToggle, false);
+
+
+    elemDiv.appendChild(clickThruDiv);
+    elemDiv.appendChild(slideIconDiv);
+
+    document.body.appendChild(elemDiv);
+
+
+    talktype(function() {
+      talktype.feedWidgetIframe(elemDiv);
+    });
+  }
+})();
+
+
